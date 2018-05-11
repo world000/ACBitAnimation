cbf887ac7803633b480854d1071c86ac37224327
Alan Chen <world000.chen@gmail.com>
2018-05-11 20:07:37 +0800

    code update.

--- ViewController.m
+++ ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
+#import "YLCoinView.h"
+
 @interface ViewController ()
+
+@property (nonatomic, strong) YLCoinBitView *bitView;
+@property (nonatomic, strong) YLCoinInnerView *bitInnerView;
+@property (nonatomic, strong) YLCoinView *coinView;
 
 @end
 
@@ -17,8 +23,150 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self testBit3View];
 }
 
+
+- (void) testBit3View {
+    YLCoinView *coinView = [[YLCoinView alloc] initWithFrame:CGRectMake(100, 200, 65, 28)];
+    self.coinView = coinView;
+    
+    [self.view addSubview:coinView];
+    
+    UIButton *resetBtn3 = [UIButton buttonWithType:UIButtonTypeSystem];
+    resetBtn3.frame = CGRectMake(200, 200, 100, 50);
+    [resetBtn3 setTitle:@"Reset" forState:UIControlStateNormal];
+    [resetBtn3 addTarget:self action:@selector(resetBtn3Tapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:resetBtn3];
+    
+    UIButton *startBtn3 = [UIButton buttonWithType:UIButtonTypeSystem];
+    startBtn3.frame = CGRectMake(200, 300, 100, 50);
+    [startBtn3 setTitle:@"Start" forState:UIControlStateNormal];
+    [startBtn3 addTarget:self action:@selector(startBtn3Tapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:startBtn3];
+}
+
+
+- (void) resetBtn3Tapped: (id) sender {
+    [self.coinView setCoinStr:@"0" duration:5 direction:NO];
+}
+
+- (void) startBtn3Tapped: (id) sender {
+    [self.coinView setCoinStr:@"4,239" duration:5 direction:YES];
+}
+
+
+
+
+- (void) testBit2View {
+    YLCoinInnerView *bitInnerView = [[YLCoinInnerView alloc] initWithFrame:CGRectMake(100, 200, 65, 28)];
+    self.bitInnerView = bitInnerView;
+    
+    [self.view addSubview:bitInnerView];
+    
+    UIButton *resetBtn2 = [UIButton buttonWithType:UIButtonTypeSystem];
+    resetBtn2.frame = CGRectMake(200, 200, 100, 50);
+    [resetBtn2 setTitle:@"Reset" forState:UIControlStateNormal];
+    [resetBtn2 addTarget:self action:@selector(resetBtn2Tapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:resetBtn2];
+    
+    UIButton *startBtn2 = [UIButton buttonWithType:UIButtonTypeSystem];
+    startBtn2.frame = CGRectMake(200, 300, 100, 50);
+    [startBtn2 setTitle:@"Start" forState:UIControlStateNormal];
+    [startBtn2 addTarget:self action:@selector(startBtn2Tapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:startBtn2];
+}
+
+
+- (void) resetBtn2Tapped: (id) sender {
+    [self.bitInnerView setCoinStr:@"0" duration:1 direction:NO];
+}
+
+- (void) startBtn2Tapped: (id) sender {
+    [self.bitInnerView setCoinStr:@"4,239" duration:1 direction:YES];
+}
+
+- (void) testBitView {
+    YLCoinBitView *bitView = [[YLCoinBitView alloc] initWithFrame:CGRectMake(100, 200, 17, 28)];
+    NSString *bitSeqStr = @" ,5";
+    [bitView setCoinList: [bitSeqStr componentsSeparatedByString:@","]];
+    [bitView setCurrentBit:@" "];
+    self.bitView = bitView;
+    
+    [self.view addSubview:bitView];
+    
+    UIButton *resetBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    resetBtn.frame = CGRectMake(200, 200, 100, 50);
+    [resetBtn setTitle:@"Reset" forState:UIControlStateNormal];
+    [resetBtn addTarget:self action:@selector(resetBtnTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:resetBtn];
+    
+    UIButton *startBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    startBtn.frame = CGRectMake(200, 300, 100, 50);
+    [startBtn setTitle:@"Start" forState:UIControlStateNormal];
+    [startBtn addTarget:self action:@selector(startBtnTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:startBtn];
+}
+
+- (void) resetBtnTapped: (id) sender {
+    NSString *bitSeqStr = @" ,5";
+    [self.bitView setCoinList: [bitSeqStr componentsSeparatedByString:@","]];
+    [self.bitView setCurrentBit:@" "];
+}
+
+- (void) startBtnTapped: (id) sender {
+    NSString *bitSeqStr = @" ,5";
+    [self.bitView setCoinList: [bitSeqStr componentsSeparatedByString:@","]];
+//    [self.bitView setCurrentBit:@"5"];
+    [self.bitView setCurrentBit:@"5" duration:1];
+}
+
+- (void) allFonts {
+    NSArray *familyNames =[[NSArray alloc] initWithArray:[UIFont familyNames]];
+    NSArray *fontNames;
+    
+    NSLog(@"[familyNames count]===%lu",(unsigned long)[familyNames count]);
+    for(NSInteger familyCount=0;familyCount<[familyNames count]; familyCount++)
+    {
+        NSLog(@"Family name: %@", [familyNames objectAtIndex:familyCount]);
+        fontNames =[[NSArray alloc]initWithArray:[UIFont fontNamesForFamilyName:[familyNames objectAtIndex:familyCount]]];
+        for(NSInteger fontCount=0; fontCount<[fontNames count]; fontCount++)
+        {
+            NSLog(@"Font name: %@",[fontNames objectAtIndex:fontCount]);
+            
+        }
+    }
+}
+
+- (void) testFont {
+//    UIFont *font = [UIFont fontWithName:@"DINAlternate-Bold" size:17]; // DINCondensed-Bold // DINAlternate & DINCondensed
+    UIFont *font = [UIFont systemFontOfSize:17];
+    
+    NSLog(@"font = %@", font);
+    
+    NSLog(@"ascender = %f", font.ascender);
+    NSLog(@"descender = %f", font.descender);
+    NSLog(@"pointSize = %f", font.pointSize);
+    NSLog(@"capHeight = %f", font.capHeight);
+    NSLog(@"xHeight = %f", font.xHeight);
+    NSLog(@"lineHeight = %f", font.lineHeight);
+    NSLog(@"leading = %f", font.leading);
+    
+    NSString *bit = @"8";
+    CGSize bitSize = [bit sizeWithAttributes:@{NSFontAttributeName: font, NSForegroundColorAttributeName: [UIColor blackColor]}];
+    NSLog(@"bitSize = %@", NSStringFromCGSize(bitSize));
+    
+    NSMutableParagraphStyle *paraStyle = [[NSMutableParagraphStyle alloc] init];
+    paraStyle.minimumLineHeight = 3;
+    paraStyle.minimumLineHeight = 3;
+    paraStyle.alignment = NSTextAlignmentJustified;
+    paraStyle.lineBreakMode = NSLineBreakByTruncatingTail;
+    
+    NSAttributedString *bitAttr = [[NSAttributedString alloc] initWithString:bit attributes:@{NSFontAttributeName: font, NSForegroundColorAttributeName: [UIColor blackColor]/*, NSParagraphStyleAttributeName: paraStyle*/}];
+    CGSize bitAttriSize = [bitAttr boundingRectWithSize:CGSizeMake(100, 100) options:0 context:nil].size;
+    NSLog(@"bitAttriSize = %@", NSStringFromCGSize(bitAttriSize));
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- YLCoinView.m
+++ YLCoinView.m
@@ -8,107 +8,7 @@
 
 #import "YLCoinView.h"
 
-@interface YLCoinBitView : UIView
-
-@property (nonatomic, strong) UILabel *barLabel;
-
-@end
-
-
 @implementation YLCoinBitView
-
-@end
-
-
-
-@interface YLCoinInnerView : UIView
-
-@property (nonatomic, strong) NSArray *bitViews;
-
-@property (nonatomic, copy) NSString *preCoinStr;
-@property (nonatomic, copy) NSString *coinStr;
-
-@property (nonatomic, assign) NSTimeInterval animationDuration;
-
-@end
-
-@implementation YLCoinInnerView
-
-- (void) setupBitViews {
-    YLCoinBitView *bitView = [[YLCoinBitView alloc] init];
-    
-    NSMutableArray *coinBitArray = [[NSMutableArray alloc] initWithCapacity:_coinStr.length];
-    for (NSInteger index = 0; index < _coinStr.length; index++) {
-        [coinBitArray addObject:[_coinStr substringWithRange:NSMakeRange(index, 1)]];
-    }
-    
-}
-
-- (void) commonSetupWithCoinStr: (NSString *) coinStr {
-    _coinStr = coinStr;
-    _animationDuration = 1;
-    
-    [self setupBitViews];
-}
-
-- (instancetype)initWithCoinStr: (NSString *)coinStr {
-    self = [super init];
-    
-    if (self) {
-        [self commonSetupWithCoinStr:coinStr];
-    }
-    
-    return self;
-}
-
-- (void) setCoinStr:(NSString *)coinStr {
-    [self setCoinStr:coinStr animated:NO];
-}
-
-- (void) setCoinStr:(NSString *)coinStr animated: (BOOL) animated {
-    if (coinStr.length == 0) {
-        return;
-    }
-    
-    if ([_coinStr isEqualToString:coinStr]) {
-        return;
-    }
-    
-    _preCoinStr = [_coinStr copy];
-    _coinStr = [coinStr copy];
-    
-    
-}
-
-
-
-@end
-
-
-
-
-
-
-
-
-
-@interface YLCoinView ()
-
-@property (nonatomic, strong) YLCoinInnerView *coinView;
-
-@end
-
-@implementation YLCoinView
-
-- (instancetype)init {
-    self = [super init];
-    
-    if (self) {
-        [self commonSetup];
-    }
-    
-    return self;
-}
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
@@ -131,24 +31,475 @@
 }
 
 - (void) commonSetup {
-    _coinView = [[YLCoinInnerView alloc] initWithCoinStr:@"0"];
+    // _font = [UIFont systemFontOfSize:17];
+    _coinList = @[@"0", @"1"];
+    _currentBit = @"0";
+    
+    _textFont = [UIFont fontWithName:@"DINAlternate-Bold" size:17]; // DINCondensed-Bold // DINAlternate & DINCondensed
+    _textColor = [UIColor blackColor]; // TODO:
+
+    self.backgroundColor = [UIColor clearColor]; // TODO:
+    self.clipsToBounds = YES; // TODO:
+
+    _barLabel = [[UILabel alloc] initWithFrame:self.bounds];
+    _barLabel.backgroundColor = [UIColor clearColor]; // TODO:
+    _barLabel.numberOfLines = 0;
+    [self addSubview:_barLabel];
+    [self updateBarLabel];
+}
+
+- (CGFloat)lineHeight {
+    return ceilf(self.textFont.lineHeight);
+}
+
+- (CGFloat) lineSpace {
+    return 5;
+}
+
+- (CGFloat) bitWidth {
+    return 15;
+}
+
+- (CGFloat) bitHeight {
+    return ([self lineHeight] + [self lineSpace]);
+}
+
+- (void)setCoinList:(NSArray *)coinList {
+    if (coinList.count == 0) {
+        return;
+    }
+    
+    if (coinList == _coinList) {
+        return;
+    }
+    
+    BOOL coinSame = YES;
+    if (_coinList.count == coinList.count) {
+        NSInteger coinListCount = _coinList.count;
+        for (NSInteger index = 0; index < coinListCount; index++) {
+            NSString *coinBit1 = _coinList[index];
+            NSString *coinBit2 = coinList[index];
+            if (![coinBit1 isEqualToString:coinBit2]) {
+                coinSame = NO;
+                break;
+            }
+        }
+    }
+    else {
+        coinSame = NO;
+    }
+    
+    if (coinSame) {
+        return;
+    }
+    
+    _coinList = coinList;
+    
+    [self updateBarLabel];
+    
+    // check whether current bit is still in coin list.
+    NSInteger foundIndex = [self indexOfBit:self.currentBit];
+    
+    if (foundIndex == NSNotFound) {
+        _currentBit = self.coinList[0]; // if not found, reset to index 0.
+    }
+    
+    [self layoutBarLabel];
+}
+
+- (void) updateBarLabel {
+    NSString *coinStr = [self.coinList componentsJoinedByString:@"\n"];
+    
+
+    NSMutableParagraphStyle *paraStyle = [[NSMutableParagraphStyle alloc] init];
+    paraStyle.minimumLineHeight = self.lineHeight;
+    paraStyle.maximumLineHeight = self.lineHeight;
+    paraStyle.lineSpacing = [self lineSpace];
+    paraStyle.alignment = NSTextAlignmentCenter;
+    paraStyle.lineBreakMode = NSLineBreakByTruncatingTail;
+    
+    NSAttributedString *coinAttrStr = [[NSAttributedString alloc] initWithString:coinStr attributes:@{NSFontAttributeName: self.textFont, NSForegroundColorAttributeName: self.textColor, NSParagraphStyleAttributeName: paraStyle}];
+    
+    self.barLabel.attributedText = coinAttrStr;
+    
+    CGSize coinAttrStrSize = [self.barLabel.attributedText boundingRectWithSize:CGSizeMake([self bitWidth], CGFLOAT_MAX) options:NSStringDrawingUsesFontLeading | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingTruncatesLastVisibleLine context:nil].size;
+    coinAttrStrSize.width = ceilf(coinAttrStrSize.width);
+    coinAttrStrSize.height = ceilf(coinAttrStrSize.height) - (self.coinList.count > 1 ? 0 : [self lineSpace]);
+    
+    CGRect barLabelRect = self.barLabel.frame;
+    barLabelRect.size = coinAttrStrSize;
+    self.barLabel.frame = barLabelRect;
+}
+
+- (void) setCurrentBit:(NSString *)currentBit {
+    [self setCurrentBit:currentBit duration:0];
+}
+
+- (void) setCurrentBit:(NSString *)currentBit duration: (NSTimeInterval) duration {
+    if (currentBit == nil) {
+        currentBit = @"";
+    }
+    
+    if ([_currentBit isEqualToString:currentBit]) {
+        return;
+    }
+    
+    NSInteger foundIndex = [self indexOfBit:currentBit];
+    
+    _currentBit = currentBit;
+    
+    CGRect barLabelRect = self.barLabel.frame;
+    CGFloat barLabelOriginY = (CGRectGetHeight(self.bounds) - [self lineHeight]) / 2.0f;
+    barLabelRect.origin.y = barLabelOriginY - foundIndex * [self bitHeight];
+    
+    if (duration > 0) {
+        [UIView animateWithDuration:duration animations:^{
+            self.barLabel.frame = barLabelRect;
+        }];
+    }
+    else {
+        self.barLabel.frame = barLabelRect;
+    }
+}
+
+- (void) layoutSubviews {
+    [super layoutSubviews];
+    
+    [self layoutBarLabel];
+}
+
+- (void) layoutBarLabel {
+    if (self.barLabel.attributedText == nil) {
+        self.barLabel.frame = CGRectZero;
+    }
+    else {
+        NSInteger foundIndex = [self indexOfBit:self.currentBit];
+        
+        if (foundIndex == NSNotFound) {
+            return;
+        }
+        
+        CGRect barLabelRect = self.barLabel.frame;
+        CGPoint barLabelOrigin = CGPointMake((CGRectGetWidth(self.bounds) - barLabelRect.size.width) / 2.0f,
+                                             (CGRectGetHeight(self.bounds) - [self lineHeight]) / 2.0f);
+        barLabelRect.origin = CGPointMake(barLabelOrigin.x, barLabelOrigin.y - foundIndex * [self bitHeight]);
+        self.barLabel.frame = barLabelRect;
+    }
+}
+
+#pragma mark - private methods
+
+- (NSInteger) indexOfBit:(NSString *) bit {
+    NSInteger foundIndex = NSNotFound;
+    for (NSInteger index = 0; index < self.coinList.count; index++) {
+        if ([self.coinList[index] isEqualToString:bit]) {
+            foundIndex = index;
+            break;
+        }
+    }
+    
+    return foundIndex;
+}
+
+- (NSString *)debugDescription {
+    NSString *debug = [[NSString alloc] initWithFormat:@"%p \n bitlist = %@ \n current = %@", &self, self.coinList, self.currentBit];
+    
+    return debug;
+}
+
+@end
+
+
+
+
+
+
+
+@implementation YLCoinInnerView
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    
+    if (self) {
+        [self commonSetup];
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        [self commonSetup];
+    }
+    
+    return self;
+}
+
+- (void) commonSetup {
+    [self setupBitViews];
+    _coinStr = @"0";
+    NSMutableArray *coinBitList = [[NSMutableArray alloc] initWithCapacity:_coinStr.length];
+    for (NSInteger index = _coinStr.length - 1; index < _coinStr.length; index--) {
+        [coinBitList addObject:[_coinStr substringWithRange:NSMakeRange(index, 1)]];
+    }
+    _coinBitList = [coinBitList copy];
+    
+    [self updateBitViewsWithDuration:0 direction:NO];
+}
+
+- (CGFloat) bitWidth {
+    return 10;
+}
+
+- (CGFloat) thousandSymbolWidth {
+    return 5;
+}
+
+- (CGFloat) viewWidth {
+    CGFloat width = 0;
+    CGFloat coinBitCount = self.coinBitList.count;
+    if (coinBitCount >= 5) {
+        width = [self bitWidth] * (coinBitCount - 1) + [self thousandSymbolWidth];
+    }
+    else {
+        width = [self bitWidth] * coinBitCount;
+    }
+    
+    return width;
+}
+
+- (void) setupBitViews {
+    const NSInteger maxBitCount = 7; // max seven bits, 999,999
+    NSMutableArray *bitViews = [[NSMutableArray alloc] initWithCapacity:maxBitCount];
+    for (NSInteger index = 0; index < maxBitCount; index++) {
+        YLCoinBitView *bitView = [[YLCoinBitView alloc] initWithFrame:CGRectZero];
+        [self addSubview:bitView];
+
+        if (index == 3) { // thounsand symbol
+            bitView.frame = CGRectMake(0, 0, [self thousandSymbolWidth], CGRectGetHeight(self.bounds));
+            [bitView setCoinList:@[@" ", @","]];
+            bitView.currentBit = @" ";
+        }
+        else {
+            bitView.frame = CGRectMake(0, 0, [self bitWidth], CGRectGetHeight(self.bounds));
+            [bitView setCoinList:@[@" ", @"0", @"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9"]];
+            bitView.currentBit = @" ";
+        }
+        
+//        bitView.backgroundColor =
+        [bitViews addObject:bitView];
+    }
+    
+    self.bitViews = [bitViews copy];
+}
+
+- (void) updateBitViewsWithDuration: (NSTimeInterval) duration direction: (BOOL) direction {
+    
+    for (NSInteger index = 0; index < self.bitViews.count; index++) {
+        YLCoinBitView *bitView = self.bitViews[index];
+        if (index < self.coinBitList.count) {
+            NSString *coinBit = self.coinBitList[index];
+            if ([coinBit isEqualToString:bitView.currentBit]) {
+                
+            }
+            else {
+                if ([coinBit isEqualToString:@","]) {
+                    if (direction) {
+                        [bitView setCoinList:@[@" ", @","]];
+                    }
+                    else {
+                        [bitView setCoinList:@[@",", @" "]];
+                    }
+                    [bitView setCurrentBit:coinBit duration:duration];
+                }
+                else {
+                    if ([bitView.currentBit isEqualToString:@" "]) {
+                        if (direction) {
+                            [bitView setCoinList:@[@" ", coinBit]];
+                        }
+                        else {
+                            [bitView setCoinList:@[coinBit, @" "]];
+                        }
+                        [bitView setCurrentBit:coinBit duration:duration];
+                    }
+                    else {
+                        [bitView setCoinList:[self generateCoinListFrom:bitView.currentBit to:coinBit withDirection:direction]];
+                        [bitView setCurrentBit:coinBit duration:duration];
+                    }
+                }
+            }
+        }
+        else {
+            if ([bitView.currentBit isEqualToString:@" "]) {
+                ;
+            }
+            else {
+                if (direction) {
+                    [bitView setCoinList:@[bitView.currentBit, @" "]];
+                }
+                else {
+                    [bitView setCoinList:@[@" ", bitView.currentBit]];
+                }
+                [bitView setCurrentBit:@" " duration:duration];
+            }
+        }
+    }
+}
+
+- (NSArray *) generateCoinListFrom: (NSString *) start to: (NSString *) end withDirection: (BOOL) direction {
+    NSInteger startInt = [start integerValue];
+    NSInteger endInt = [end integerValue];
+    NSMutableArray *coinList = [[NSMutableArray alloc] initWithCapacity:10];
+    while (startInt <= endInt) {
+        [coinList addObject:[@(startInt) stringValue]];
+        
+        if (startInt == endInt && coinList.count > 0) {
+            break;
+        }
+        
+        if (direction) {
+            startInt++;
+            if (startInt == 10) {
+                startInt = 0;
+            }
+        }
+        else {
+            startInt--;
+            if (startInt == 0) {
+                startInt = 9;
+            }
+        }
+    }
+    
+    return coinList;
 }
 
 - (void) setCoinStr:(NSString *)coinStr {
-    [self setCoinStr:coinStr animated:NO];
+    [self setCoinStr:coinStr duration:0 direction:NO];
 }
 
-- (void) setCoinStr:(NSString *)coinStr animated: (BOOL) animated {
-    [self.coinView setCoinStr:coinStr animated:animated];
+- (void) setCoinStr:(NSString *)coinStr duration: (NSTimeInterval) duration direction: (BOOL) direction {
+    if (coinStr.length == 0) {
+        coinStr = @"0";
+    }
     
-    if (animated) {
-        [UIView animateWithDuration:self.coinView.animationDuration animations:^{
+    if ([_coinStr isEqualToString:coinStr]) {
+        return;
+    }
+    
+    _coinStr = [coinStr copy];
+    
+    NSMutableArray *coinBitList = [[NSMutableArray alloc] initWithCapacity:_coinStr.length];
+    for (NSInteger index = _coinStr.length - 1; index < _coinStr.length; index--) {
+        [coinBitList addObject:[_coinStr substringWithRange:NSMakeRange(index, 1)]];
+    }
+    self.coinBitList = [coinBitList copy];
+    
+    [self updateBitViewsWithDuration:duration direction:direction];
+}
+
+- (void) layoutSubviews {
+    [super layoutSubviews];
+    
+    for (NSInteger index = 0; index < self.bitViews.count; index++) {
+        YLCoinBitView *bitView = self.bitViews[index];
+        if (index == 0) {
+            CGRect bitViewRect = bitView.frame;
+            bitViewRect.origin = CGPointMake(CGRectGetMaxX(self.bounds) - CGRectGetWidth(bitViewRect), 0);
+            bitView.frame = bitViewRect;
+        }
+        else {
+            YLCoinBitView *prevBitView = self.bitViews[index - 1];
+            CGRect prevBitViewRect = prevBitView.frame;
+            CGRect bitViewRect = bitView.frame;
+            bitViewRect.origin = CGPointMake(CGRectGetMinX(prevBitViewRect) - CGRectGetWidth(bitViewRect), 0);
+            bitView.frame = bitViewRect;
+        }
+    }
+}
+
+
+@end
+
+
+
+
+
+
+
+
+
+@interface YLCoinView ()
+
+@property (nonatomic, strong) YLCoinInnerView *coinView;
+
+@end
+
+@implementation YLCoinView
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    
+    if (self) {
+        [self commonSetup];
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        [self commonSetup];
+    }
+    
+    return self;
+}
+
+- (void) commonSetup {
+    _coinView = [[YLCoinInnerView alloc] initWithFrame:self.bounds];
+    [self addSubview:_coinView];
+    
+    self.backgroundColor = [UIColor redColor];
+}
+
+- (void) setCoinStr:(NSString *)coinStr {
+    [self setCoinStr:coinStr duration:0 direction:NO];
+}
+
+- (void) setCoinStr:(NSString *)coinStr duration: (NSTimeInterval) duration direction: (BOOL) direction {
+    [self.coinView setCoinStr:coinStr duration:duration direction: direction];
+    
+    CGRect coinViewRect = self.coinView.frame;
+    CGPoint upperRight = CGPointMake(CGRectGetMaxX(coinViewRect), 0);
+    coinViewRect.size = CGSizeMake([self.coinView viewWidth], CGRectGetHeight(self.bounds));
+    coinViewRect.origin = CGPointMake(upperRight.x - coinViewRect.size.width, 0);
+    self.coinView.frame = coinViewRect;
+    
+    if (duration > 0) {
+        [UIView animateWithDuration:duration animations:^{
             self.coinView.center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
         }];
     }
     else {
         self.coinView.center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
     }
+}
+
+- (void) layoutSubviews {
+    [super layoutSubviews];
+    
+    [self layoutCoinView];
+    self.coinView.center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
+}
+
+- (void) layoutCoinView {
+    CGRect coinViewRect = self.coinView.frame;
+    coinViewRect.size = CGSizeMake([self.coinView viewWidth], CGRectGetHeight(self.bounds));
+    self.coinView.frame = coinViewRect;
 }
 
 @end
